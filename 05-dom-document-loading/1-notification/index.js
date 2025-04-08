9d5992107563024a314a0e72379b7a16d319242e
cheriko <muglova@wr.company>
2025-04-08 14:43:29 +0300

    5, notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,42 @@
 export default class NotificationMessage {
+    static lastShownComponent;
+    element = document.createElement("div");
+    duration = 0;
 
+    constructor(message, options) {
+        this.duration = options?.duration;
+        this.element.className = `notification ${options?.type}`;
+        this.element.innerHTML = `
+            <div class="timer"></div>
+            <div class="inner-wrapper">
+                <div class="notification-header">${options?.type}</div>
+                <div class="notification-body">
+                    ${message}
+                </div>
+            </div>
+        `;
+        this.element.style.setProperty("--value", this.duration + "ms");
+    }
+
+    show(body = document.body) {
+        if (NotificationMessage.lastShownComponent) {
+            NotificationMessage.lastShownComponent.remove();
+        }
+        NotificationMessage.lastShownComponent = this;
+
+        body.append(this.element);
+
+        this.timerId = setTimeout(() => {
+            this.remove();
+        }, this.duration);
+    }
+
+    remove() {
+        this.element.remove();
+    }
+
+    destroy() {
+        clearTimeout(this.timerId);
+        this.remove();
+    }
 }
